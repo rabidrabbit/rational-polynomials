0e19fa683cc1e31c27b22e105fbe5bfd3ae7ea78
Brian Ha <brian@boardpackager.com>
2019-12-27 22:56:50 -0500

    Initial commit; polynomial representation started

---
+++ polynomial.py
@@ -0,0 +1,19 @@
+"""
+polynomial.py
+
+Symbolic representation of polyonmials
+"""
+
+class Polynomial:
+    def __init__(self, degree, coefficients):
+        self.degree = degree
+        self.coefficients = coefficients
+    
+    def find_hcf(self):
+        return
+    
+    def convert_to_primitive(self, degree):
+        return
+    
+    def is_primitive(self):
+        return
